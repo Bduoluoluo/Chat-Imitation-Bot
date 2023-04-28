6a44113e02de66aa6d252d6a448347475b6c3e7d
Bduoluoluo <31810717+Bduoluoluo@users.noreply.github.com>
2023-04-28 09:33:08 +0800

    Add files via upload
    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from flask import Flask, request
+from api import reveiveMes
+from imitateChat import startChat
+
+app = Flask(__name__)
+
+botStart = False
+
+# '''监听端口，获取QQ信息'''
+@app.route('/', methods = ["POST"])
+def post_data ():
+    global botStart
+    requText = request.get_json()
+    mid = requText.get('message_id')
+    if requText.get('post_type') == 'message':
+        meg_type = requText.get('message_type')
+        uid = requText.get('sender').get('user_id')
+        msg = requText.get('raw_message')
+        # 接收消息
+        if meg_type == 'group':
+            rol = requText.get('sender').get('role')
+            gid = requText.get('group_id')
+            reveiveMes(msg, uid, gid, rol, mid)
+    if (botStart is False):
+        botStart = True
+        startChat()
+    return "OK"
+
+if __name__ == '__main__':
+    app.run(debug = True, host = '127.0.0.1', port = 5701)
+
